1624b10df81d266251e8580a3b96c9e1146e5210
KorobejnikovaE <korobejnikova_ea@edu.surgu.ru>
2022-05-29 20:01:39 +0500

    8

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ import threading
 import dateparser
 import re
 import Login_info as LI
+from mysql.connector import connect, Error
 #from timefhuman import timefhuman
 from dotenv import load_dotenv
-load_dotenv()
-vk = vk_api.VkApi(token=LI.token)
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+load_dotenv()
+print("привет")
+vk = vk_api.VkApi(token=LI.token)
 longpoll = VkLongPoll(vk)
-from mysql.connector import connect, Error
+
 host = os.getenv('host')
 port = os.getenv('port')
 user = os.getenv('user')
@@ -22,11 +26,13 @@ charset = os.getenv('charset')
 
 #connection = pymysql.connect(host=host, user=user, password=password, db=database, charset=charset, cursorclass=pymysql.cursors.DictCursor)
 #database connection
-connection = pymysql.connect(host="localhost",user="root",passwd="root",database="vkbotpr" )
-cursor = connection.cursor()
-# some other statements with the help of cursor
-connection.close()
-print("с базой есть соединения")
+with connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="vkbotpr",
+) as connection:
+    print("с базой есть соединения")
 
 # Таймер для выполнения
 #
@@ -64,23 +70,25 @@ for event in longpoll.listen():
                with connect (
                        host="localhost",
                        user="root",
-                       passwd="root",
+                       password="root",
                        database="vkbotpr",
                ) as connection:
                     print("\n Успешное соединение")
+                    n = 0
                     query= "SELECT * FROM users WHERE user_id =" + str(event.user_id)
+                    print("Запрос №1: " + query)
                     with connection.cursor() as cursor:
                         cursor.execute (query)
-                        n = cur.rowcount()
+                        result = cursor.fetchall()
+                        n = len(result)
                     if n == 0:
-                        cur = connection.cursor()
-                        query= "INSERT INTO users (first_name, last_name, user_id, login) VALUES (%s, %s, %s, %s)"
-
-                        with connection.cursor() as cursor:
+                        query = "INSERT INTO users (fist_name, last_name, user_id, login) VALUES (%s, %s, %s, %s)"
+                        print("Запрос №2: " + query)
+                        with connection.cursor(buffered=True) as cursor:
                             cursor.execute (query, (user["first_name"], user["last_name"], user["id"], user['screen_name']))
             except Error as e:
-                print("я сказал:" + str(e))
-            a = input("dffdsfsdg")
+                print("Ошибка соединения №1: " + str(e))
+            #a = input("dffdsfsdg")
 
 
 
@@ -104,35 +112,67 @@ for event in longpoll.listen():
            #      connection.commit()
            #      cur.close()
             # Получение id пользователя - добавленного или имеющегося
-            cur = connection.cursor()
-            cur.execute("SELECT * FROM users WHERE vk_id=" + str(user["id"]))
-            rows = cur.fetchall()
+            rows = []
+            try:
+                with connect(
+                    host="localhost",
+                    user="root",
+                    password="root",
+                    database="vkbotpr",
+                ) as connection:
+                    query = "SELECT * FROM users WHERE users.user_id=" + str(user["id"])
+                    print("Запрос №3: " + query)
+                    with connection.cursor(buffered=True) as cursor:
+                        cursor.execute(query)
+                        #cur = connection.cursor()
+                        #cur.execute()
+                        rows = cursor.fetchall()
+            except Error as e:
+                print("Ошибка соединения №2: " + str(e))
+
             for row in rows:
-                id_user = row['id']
-            print('user_id')
+                id_user = row['user_id']
+                print(row['user_id'])
             vk.method('messages.send', {'peer_id': event.user_id, 'message': 'Приветствую Вас, ' + user["first_name"] + '! Я бот!',
                                         'random_id': random.randint(0, 1000)})
             # Текст сообщения пользователя
             request = str(event.text)
             # Массив элементов команды
             params = re.split(";|,",request)
-            if params[0] in {'','statements', 'add', 'добавить'}:
-                cur = connection.cursor()
-                # Добавляем заявление в таблицу
-                cur.execute("INSERT INTO  (name, deadline, statements, user_id) VALUES (%s, %s, %s, %s)",
-                            (params[1], params[2], params[3].strip(' '), user["id"]))
-                connection.commit()
-                cur.close()
-            else:
-                cur = connection.cursor()
-                cur.execute("SELECT * FROM users,statements WHERE statements.user_id = users.id and vk_id=" + str(user["id"]))
-                rows = cur.fetchall()
-                msg = ''
-                for row in rows:
-                    print(row)
-                    msg = msg + str(row['statements.id']) + ' ' + row['name'] + ' ' + str(row['deadline']) + '\n'
-                vk.method('messages.send',
-                          {'peer_id': event.user_id, 'message': msg, 'random_id': random.randint(0, 1000)})
+            try:
+                with connect(
+                    host="localhost",
+                    user="root",
+                    password="root",
+                    database="vkbotpr",
+                ) as connection:
+                    with connection.cursor(buffered=True) as cursor:
+                        if params[0] in ['','statements', 'add', 'добавить']:
+                            #cur = connection.cursor()
+                            # Добавляем заявление в таблицу
+                            query = "INSERT INTO statements (name, deadline, statements, user_id) VALUES (%s, %s, %s, %s)"
+                            print("Запрос №4.1: "+ query)
+                            cursor.execute(query,
+                                        (params[1], params[2], params[3].strip(' '), user["id"]))
+                            #connection.commit()
+                            #cur.close()
+                        else:
+                        #    cur = connection.cursor()
+                            query = "SELECT * FROM users, statements WHERE statements.user_id = " + str(user["id"]) + " and users.user_id = " + str(user["id"])
+                            print("Запрос №4.2: "+ query)
+                            cursor.execute(query)
+                            rows = cursor.fetchall()
+                            print("Результат запроса №4.2: " + str(rows))
+                            msg = 'Что-то пошло не так...'
+                            for row in rows:
+                                print(row)
+                                msg = msg + str(row['statements.id']) + ' ' + row['name'] + ' ' + str(row['deadline']) + '\n'
+                            vk.method('messages.send',
+                                      {'peer_id': event.user_id, 'message': msg, 'random_id': random.randint(0, 1000)})
+            except Error as e:
+                print("Ошибка соединения №3: " + str(e))
+
+
 
 #for event in longpoll.listen():
 #    if event.type == VkEventType.MESSAGE_NEW:
